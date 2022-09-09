3c66388dc63daa1a0b3d1bd17ed6741fec557566
aznd <80119822+aznd@users.noreply.github.com>
2022-09-09 13:35:12 +0200

    feat(fish): startx on tty login

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/user/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
+
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
