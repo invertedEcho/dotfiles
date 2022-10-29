e65fb65d0920c5171fb9dc8ca4a82d28b12e9d35
invertedEcho <80119822+invertedEcho@users.noreply.github.com>
2022-10-29 19:39:47 +0200

    fix: nvim startup deprecated function and add java.lua

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -115,7 +115,7 @@ end
 M.on_attach = on_attach
 
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local servers = {
 	"cssls",
