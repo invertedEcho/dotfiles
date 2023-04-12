6e35cc3292ccebaac1bac54b86998b49194d6cbe
invertedEcho <jakob.stechow@protonmail.com>
2023-04-12 23:55:17 +0200

    fish changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/user/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+set BROWSER /usr/bin/firefox
