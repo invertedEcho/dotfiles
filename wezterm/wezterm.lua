d299d87d53714fb4ca83af10b4b1d9589cead3d5
Jakob Stechow <jakob.stechow@protonmail.com>
2023-06-22 17:49:35 +0200

    some fancy wezterm stuff

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,8 +4,9 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "tokyonight"
-config.enable_tab_bar = false
-config.show_new_tab_button_in_tab_bar = false
+config.color_scheme = "Hardcore"
 config.default_cursor_style = "BlinkingUnderline"
+config.hide_tab_bar_if_only_one_tab = true
+config.scrollback_lines = 5000
+config.window_background_opacity = 0.8
 return config
