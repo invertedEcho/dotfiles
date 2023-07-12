10d4bb9d2175939815bde26ac66357d36758bbc1
Jakob Stechow <jakob.stechow@protonmail.com>
2023-07-12 03:10:57 +0200

    chore(wezterm): return whole config object instead of setting attributes manually

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,9 @@
 local wezterm = require("wezterm")
-local config = {}
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
 
-config.color_scheme = "Hardcore"
-config.default_cursor_style = "BlinkingUnderline"
-config.hide_tab_bar_if_only_one_tab = true
-config.scrollback_lines = 5000
-config.window_background_opacity = 0.8
-return config
+return {
+  color_scheme = "tokyonight",
+  default_cursor_style = "BlinkingUnderline",
+  hide_tab_bar_if_only_one_tab = true,
+  scrollback_lines = 5000,
+  warn_about_missing_glyphs = false,
+}
