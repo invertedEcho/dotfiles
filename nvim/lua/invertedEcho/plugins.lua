1d76289b525853f7bede6be07cfce393c84d0791
Jakob Stechow <jakob.stechow@protonmail.com>
2023-08-16 22:48:36 +0200

    nvim: remove comments

--- nvim/lua/invertedEcho/plugins.lua
+++ nvim/lua/invertedEcho/plugins.lua
@@ -93,11 +93,7 @@ local M = {
   {
     'folke/zen-mode.nvim',
     lazy = false,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
+    opts = {},
   },
 }
 
