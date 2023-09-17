d66909e68e21fe03a6607f94f567d9c7aa9f744c
Jakob Stechow <jakob.stechow@protonmail.com>
2023-09-17 02:50:46 +0200

    wezterm: fix macos movement by word and increase font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,5 +7,11 @@ return {
   scrollback_lines = 5000,
   warn_about_missing_glyphs = false,
   font = wezterm.font("Iosevka Nerd Font", {weight="Medium", stretch="Normal", style="Normal"}),
-  font_size = 14,
+  font_size = 18,
+  keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  }
 }
