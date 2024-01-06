6c887239cbb03a6c3d524a72f793253be33f98b5
Jakob Stechow <jakob.stechow@pm.me>
2024-01-06 21:34:31 +0100

    fix: decrease wezterm font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ return {
 	scrollback_lines = 5000,
 	warn_about_missing_glyphs = false,
 	font = wezterm.font("Iosevka Nerd Font Mono", { weight = "Medium", stretch = "Normal", style = "Normal" }),
-	font_size = 16,
+	font_size = 14,
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	keys = {
 		{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
