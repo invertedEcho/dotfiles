8f0cac29992274a89eb06332a8899dcafbf04b02
Jakob Stechow <jakob.stechow@pm.me>
2024-01-06 21:34:18 +0100

    feat: markdown-preview and auto clouse buffers plugin for nvim

--- nvim/lua/invertedEcho/plugins.lua
+++ nvim/lua/invertedEcho/plugins.lua
@@ -127,6 +127,19 @@ local M = {
       require('colorizer').setup()
     end,
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+  {
+    'axkirillov/hbac.nvim',
+    config = true,
+  },
 }
 
 return M
