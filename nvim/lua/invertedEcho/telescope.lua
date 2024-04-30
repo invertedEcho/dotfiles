48289197d08f117d0de1b54114bc5cebbfd47c84
Jakob Stechow <jakob.stechow@pm.me>
2024-04-30 18:51:21 +0200

    fix(nvim): telescope hotkeys

--- nvim/lua/invertedEcho/telescope.lua
+++ nvim/lua/invertedEcho/telescope.lua
@@ -10,4 +10,9 @@ SetKey('n', '<leader>tf', '<cmd>Telescope find_files<cr>')
 SetKey('n', '<leader>tt', '<cmd>Telescope live_grep<cr>')
 SetKey('n', '<leader>ty', '<cmd>Telescope resume<cr>')
 SetKey('n', '<leader>tc', '<cmd>Telescope colorscheme<cr>')
-SetKey('n', '<leader>tg', '<cmd>Telescope find_files hidden=true<cr>')
+SetKey(
+  'n',
+  '<leader>th',
+  '<cmd>Telescope find_files hidden=true no_ignore=true<cr>'
+)
+SetKey('n', '<leader>tb', '<cmd>Telescope buffers<cr>')
