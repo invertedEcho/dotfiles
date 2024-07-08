3827f241039a0ac5b76438882b0213d989d7609a
Jakob Stechow <jakob.stechow@pm.me>
2024-07-08 15:26:21 +0200

    feat(hypr): scratchpad, couple of useful floating windowrules, drop edit nixos config script because its pretty bad

--- scripts/edit-nixos-configs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-BASE_DOTFILES_DIR="/home/$USER/dev/priv/dotfiles"
-NIXOS_DIR="$BASE_DOTFILES_DIR/nixos"
-
-kitty nvim "$NIXOS_DIR"
-kitty sudo nixos-rebuild switch --flake "$NIXOS_DIR"/.#default
-kitty lazygit -p "$BASE_DOTFILES_DIR"
