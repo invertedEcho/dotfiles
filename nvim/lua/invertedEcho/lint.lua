bc44322e09f1103d343396a47573cf97e7861278
Jakob Stechow <jakob.stechow@pm.me>
2024-07-17 21:59:27 +0200

    fix(nvim): remove unsupported linters

--- nvim/lua/invertedEcho/lint.lua
+++ nvim/lua/invertedEcho/lint.lua
@@ -1,8 +1,5 @@
 require('lint').linters_by_ft = {
   python = { 'flake8' },
-  typescript = { ' eslint ' },
-  typescriptreact = { 'eslint' },
-  nix = { 'nix' },
 }
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
   callback = function()
