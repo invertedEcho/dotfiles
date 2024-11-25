2ecf31049b37a89bd7cb4f8b97196c60418533c1
Jakob Stechow <jakob.stechow@pm.me>
2024-11-25 09:29:46 +0100

    fix(scripts/monitors): enable laptop screen per default

--- scripts/setup-monitors.sh
+++ scripts/setup-monitors.sh
@@ -4,12 +4,19 @@ LAPTOP_SCREEN_NAME=eDP-1
 
 ALL_MONITORS=$(hyprctl -j monitors all)
 
+# COUNT_OF_MONITORS=$(echo "$ALL_MONITORS" | jq length)
+# if [ "$COUNT_OF_MONITORS" = "1" ]; then
+#   hyprctl keyword monitor eDP-1,preferred,auto,1
+#   notify-send "Successfully setup laptop screen"
+#   exit;
+# fi
+
 OUTPUT=$(echo "$ALL_MONITORS" | jq --arg laptop_screen_name "$LAPTOP_SCREEN_NAME" -r '.[] | select(.name == $laptop_screen_name) | .name')
 
 if [ -n "$OUTPUT" ]; then
-  hyprctl keyword monitor desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 23082B003536, 2560x1440@144,2560x0,1
+  hyprctl keyword monitor desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 23082B003536, 2560x1440,2560x0,1
   hyprctl keyword monitor desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 23082B003522, 2560x1440,0x0,1
-  hyprctl keyword monitor $LAPTOP_SCREEN_NAME, disable
+  hyprctl keyword monitor eDP-1,preferred,auto,1
   notify-send "Successfully setup monitors for Office."
 else
   hyprctl keyword monitor desc:Dell Inc. DELL U2415 7MT0177A2T2S,highres,0x0,1
