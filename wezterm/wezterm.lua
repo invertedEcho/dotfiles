63341b35cc03d1ab46ff92633f1b304b99961215
Jakob Stechow <jakob.stechow@pm.me>
2025-03-07 10:01:55 +0100

    feat!: switch from kitty to wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Gruvbox Dark (Gogh)",
+	font = wezterm.font("Terminess Nerd Font Mono"),
+	font_size = 18,
+	front_end = "WebGpu",
+	hide_tab_bar_if_only_one_tab = true,
+}
