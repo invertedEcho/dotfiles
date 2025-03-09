58e5192c71d4a0d0c922a0aba0375baf9db0f93b
Jakob Stechow <jakob.stechow@pm.me>
2025-03-09 10:19:31 +0100

    fix(wezterm): smaller font size for linux

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,11 @@
 local wezterm = require("wezterm")
+local utils = require("utils")
 
 return {
 	color_scheme = "Gruvbox Dark (Gogh)",
 	font = wezterm.font("Terminess Nerd Font Mono"),
-	font_size = 18,
+	font_size = utils.is_linux() and 12 or 18,
 	front_end = "WebGpu",
 	hide_tab_bar_if_only_one_tab = true,
+	enable_wayland = false,
 }
