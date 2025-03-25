c1ab8f86bef41d995acda3311a5519a99698d603
Jakob Stechow <jakob.stechow@pm.me>
2025-03-25 15:24:24 +0100

    fix(wezterm): only specify font on linux

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,11 +3,13 @@ local utils = require("utils")
 
 return {
 	color_scheme = "Gruvbox Dark (Gogh)",
-	font = wezterm.font({
-		family = "JetBrains Mono Nerd Font Mono",
-		-- disable ligatures
-		harfbuzz_features = { "calt=0", "clig0", "liga=" },
-	}),
+	font = utils.is_linux()
+			and wezterm.font({
+				family = "JetBrains Mono Nerd Font Mono",
+				-- disable ligatures
+				harfbuzz_features = { "calt=0", "clig0", "liga=" },
+			})
+		or nil,
 	font_size = utils.is_linux() and 11 or 18,
 	front_end = "WebGpu",
 	hide_tab_bar_if_only_one_tab = true,
