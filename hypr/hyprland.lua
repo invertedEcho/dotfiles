45fdc61ada95fe6d7d6675f8b9901852b475ef7a
Jakob Stechow <jakob.stechow@pm.me>
2026-05-30 13:37:47 +0200

    update

---
+++ hypr/hyprland.lua
@@ -0,0 +1,20 @@
+hl.monitor({
+  output = "eDP-1",
+  mode = "preferred",
+  position = "auto",
+  scale = 1
+})
+
+hl.monitor({
+  output = "DP-2",
+  mode = "highrr",
+  position = "1200x0",
+  scale = 1
+})
+
+hl.monitor({
+  output = "DP-3",
+  mode = "1920x1200",
+  position = "0x0",
+  scale = 1
+})
