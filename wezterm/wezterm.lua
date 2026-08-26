9ba5225e00632a4f49284049008d25fd3ae49312
Jakob Stechow <jakob.stechow@pm.me>
2026-08-26 22:04:48 +0200

    keybind to fzf all links in tmux, add titlebar for wezterm on gnome

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,4 +8,9 @@ config.font_size = 14
 
 config.hide_tab_bar_if_only_one_tab = true
 
+-- Both these configs are required to have a normal titlebar in GNOME.
+-- https://github.com/wezterm/wezterm/issues/4962
+config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
+config.enable_wayland = false
+
 return config
